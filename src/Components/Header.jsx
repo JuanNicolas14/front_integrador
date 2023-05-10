31cd14c969e2c24eddcc62c8d174aaf239d55c18
JuanNicolas14 <juan.cardenasc@outlook.com>
2023-05-10 18:26:24 -0500

    Commit 2 - Agregue menu movil y solucione algunos problemas del responsive

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
 import images from '../assets/images/images'
+import {HiMenu} from 'react-icons/hi'
+import {GrClose} from 'react-icons/gr'
 
 const Header = () => {
+
+  const [show, setShow] = useState(false)
+
+  const showMenu = ()=> {
+    if(show){
+      setShow(false)
+    }else{
+      setShow(true)
+    }
+  }
   return (
     <header>
       <section>
@@ -17,6 +29,33 @@ const Header = () => {
           <button>Crear cuenta</button>
           <button>Iniciar sesión</button>
         </div>
+        {show 
+        ? <div className='menu-icono'>
+            <button onClick={showMenu}><HiMenu /></button>
+          </div>
+        : <div className='menu-movil'>
+            <div className="menu">
+              <div className='menu-container'>
+                <button onClick={showMenu}><GrClose/></button>
+                <h2>Menú</h2>
+              </div>
+              <ul>
+                <li>Crear cuenta</li>
+                <li>Iniciar sesión</li>
+              </ul>
+            </div>
+            <div className="redes-menu">
+              <img
+                src={images.facebook}
+                alt="icono-facebook"
+              />
+              <img className='logo' src={images.linkedin} alt="icono-linkedin" />
+              <img src={images.twitter} alt="icono-twitter" />
+              <img className='logo' src={images.instagram} alt="icono-instagram" />
+            </div>
+          </div>
+        }
+        
       </section>
     </header>
   )
